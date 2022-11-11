75484cc49a14779e8d1e9937db7d252c47b9c100
danghung <hungdang02042003@gmail.com>
2022-11-11 19:22:29 +0700

    style: :lipstick: design index page XOSS fashion
    q
    ear

---
+++ public/layout/javascript/detail.js
@@ -0,0 +1,44 @@
+// Switch Tab
+const tab_list = document.querySelectorAll(".detail__tablist-container li");
+const divs = document.querySelectorAll(".detail__reviews-description > div")
+
+divs.forEach((e, index) => { 
+    if(index != 0)
+        e.style.display = 'none' 
+})
+
+tab_list.forEach((value,key) => {
+    value.addEventListener('click', (e) => {
+        e.preventDefault()    
+        tab_list.forEach((e) => { e.classList.remove('active')})
+        divs.forEach((e) => { e.style.display = 'none' })
+        value.classList.add('active')
+        divs[key].style.display = 'block';
+    })
+})
+
+
+// change quantity
+const input_quantity = document.querySelector(".detail_product-input-plus-minus")
+const btn_inc = document.querySelector(".detail__product-inc")
+const btn_dec = document.querySelector(".detail__product-dec")
+
+
+
+btn_inc.addEventListener('click', () => {
+
+    let value = input_quantity.value
+    input_quantity.value = parseInt(value) + 1;
+})
+
+btn_dec.addEventListener('click', () => {
+    let value = input_quantity.value
+    if(parseInt(value) > 0) {
+        input_quantity.value = parseInt(value) - 1;
+    }
+})
+
+
+
+
+
--- public/layout/javascript/slick-slider.js
+++ public/layout/javascript/slick-slider.js
@@ -1,43 +1,209 @@
-$(".footer__brands").slick({
+(function () {
+   $(".footer__brands").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 3,
+               infinite: true,
+               dots: false,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            },
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            },
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            },
+         },
+         // You can unslick at a given breakpoint now by adding:
+         // settings: "unslick"
+         // instead of a settings object
+      ],
+   });
+})();
+
+// if ($(".index__banner-list")) {
+//    $(".index__banner-list").slick({
+//       dots: false,
+//       rtl: true,
+//       infinite: true,
+//       speed: 300,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       autoplay: true,
+//       autoplaySpeed: 2000,
+//       responsive: [
+//          {
+//             breakpoint: 1024,
+//             settings: {
+//                slidesToShow: 3,
+//                slidesToScroll: 3,
+//                infinite: true,
+//                dots: false,
+//                autoplay: true,
+//                autoplaySpeed: 2000,
+//             },
+//          },
+//          {
+//             breakpoint: 600,
+//             settings: {
+//                slidesToShow: 2,
+//                slidesToScroll: 2,
+//                autoplay: true,
+//                autoplaySpeed: 2000,
+//             },
+//          },
+//          {
+//             breakpoint: 480,
+//             settings: {
+//                slidesToShow: 2,
+//                slidesToScroll: 2,
+//                autoplay: true,
+//                autoplaySpeed: 2000,
+//             },
+//          },
+//       ],
+//    });
+// }
+
+if ($(".feature__list")) {
+   $(".feature__list").slick({
+      dots: false,
+      useCSS: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1000,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 3,
+               slidesToScroll: 3,
+               infinite: true,
+               dots: false,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            },
+         },
+         {
+            breakpoint: 600,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            },
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+               autoplay: true,
+               autoplaySpeed: 2000,
+            },
+         },
+      ],
+   });
+}
+
+$(".detail__product-slider").slick({
    dots: false,
-   infinite: true,
-   speed: 300,
-   slidesToShow: 5,
-   slidesToScroll: 1,
-   autoplay: true,
-   autoplaySpeed: 2000,
+   slidesToShow: 4,
+   slidesToScroll: 4,
+   Infinity: true,
    responsive: [
       {
-         breakpoint: 1024,
+         breakpoint: 1250,
          settings: {
             slidesToShow: 3,
             slidesToScroll: 3,
             infinite: true,
             dots: false,
-            autoplay: true,
-            autoplaySpeed: 2000,
          },
       },
       {
-         breakpoint: 600,
+         breakpoint: 950,
          settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
          },
       },
       {
-         breakpoint: 480,
+         breakpoint: 650,
          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            autoplay: true,
-            autoplaySpeed: 2000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
          },
       },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
    ],
+});
+
+
+
+$(".detail__product-slider").slick({
+   dots: false,
+   slidesToShow: 4,
+   slidesToScroll: 4,
+   Infinity: true,
+   responsive: [
+      {
+         breakpoint: 1250,
+         settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false,
+         },
+      },
+      {
+         breakpoint: 950,
+         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+         },
+      },
+      {
+         breakpoint: 650,
+         settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+         },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+   ],
+   prevArrow:
+        "<button type='button' class='slick-prev slick-btn'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:
+        "<button type='button' class='slick-next slick-btn'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 });
