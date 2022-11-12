171988b609e84eed8c27821e53ddae0ecc202b99
DevidMonster <hoadn@DESKTOP-GI06QKV>
2022-11-12 23:41:30 +0700

    Detail Product

--- public/layout/javascript/detail.js
+++ public/layout/javascript/detail.js
@@ -2,20 +2,24 @@
 const tab_list = document.querySelectorAll(".detail__tablist-container li");
 const divs = document.querySelectorAll(".detail__reviews-description > div")
 
-divs.forEach((e, index) => { 
-    if(index != 0)
-        e.style.display = 'none' 
-})
-
-tab_list.forEach((value,key) => {
-    value.addEventListener('click', (e) => {
-        e.preventDefault()    
-        tab_list.forEach((e) => { e.classList.remove('active')})
-        divs.forEach((e) => { e.style.display = 'none' })
-        value.classList.add('active')
-        divs[key].style.display = 'block';
+if(divs) {
+    divs.forEach((e, index) => { 
+        if(index != 0)
+            e.style.display = 'none' 
     })
-})
+}
+
+if(tab_list) {
+    tab_list.forEach((value,key) => {
+        value.addEventListener('click', (e) => {
+            e.preventDefault()    
+            tab_list.forEach((e) => { e.classList.remove('active')})
+            divs.forEach((e) => { e.style.display = 'none' })
+            value.classList.add('active')
+            divs[key].style.display = 'block';
+        })
+    })
+}
 
 
 // change quantity
@@ -24,21 +28,20 @@ const btn_inc = document.querySelector(".detail__product-inc")
 const btn_dec = document.querySelector(".detail__product-dec")
 
 
+if(input_quantity) {
+    btn_inc.addEventListener('click', () => {
 
-btn_inc.addEventListener('click', () => {
+        let value = input_quantity.value
+        input_quantity.value = parseInt(value) + 1;
+    })
 
-    let value = input_quantity.value
-    input_quantity.value = parseInt(value) + 1;
-})
-
-btn_dec.addEventListener('click', () => {
-    let value = input_quantity.value
-    if(parseInt(value) > 0) {
-        input_quantity.value = parseInt(value) - 1;
-    }
-})
-
-
+    btn_dec.addEventListener('click', () => {
+        let value = input_quantity.value
+        if(parseInt(value) > 0) {
+            input_quantity.value = parseInt(value) - 1;
+        }
+    })
+}
 
 
 
