a6661909576c0972ccdc685f491e3c4363c4c3fb
danghung <hungdang02042003@gmail.com>
2022-11-16 20:27:09 +0700

    làm phần giao diện home admin

---
+++ admin/controllers/c_home.php
@@ -0,0 +1,19 @@
+<?php
+
+@session_start();
+
+include('models/m_home.php');
+
+class c_home
+{
+    public function show()
+    {
+        $m_home = new m_home();
+        $count_products = $m_home->count_products();
+        $count_customers = $m_home->count_customers();
+        $count_product_categories = $m_home->count_product_categories();
+        $count_comment = $m_home->count_comment();
+        $view = 'views/home/v_home.php';
+        include('templates/admin/layout.php');
+    }
+}
