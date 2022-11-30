173dc361abdcd90bbc72405f0d7da1644521c2fb
danghung <hungdang02042003@gmail.com>
2022-11-30 20:12:14 +0700

    update thêm phần tính năng quản lí giỏ hàng

--- admin/views/orders/v_orders.php
+++ admin/views/orders/v_orders.php
@@ -23,11 +23,6 @@ if (isset($_GET['error'])) {
 
     <!-- container main -->
     <main class="container__main">
-        <div class="container__main-handler">
-            <div class="container__main-link">
-
-            </div>
-        </div>
         <div class="container__table">
             <table>
                 <tr>
@@ -44,16 +39,16 @@ if (isset($_GET['error'])) {
                     <td><?= $each->name_customer; ?></td>
                     <td class="container__table-desc-parent">
                         <div class="container__table-desc">
-                            <p>địa chỉ:<?= $each->address ?> || sđt:<?= $each->phone_number?></p>
+                            <p>địa chỉ:<?= $each->address ?> || sđt:<?= $each->phone_number ?></p>
                         </div>
                     </td>
                     <td>$ <?= $each->total; ?>.00</td>
                     <td>
-                        <a href="order-detail.php?id_order=<?= $each->id; ?>">
-                            Xem chi tiết
+                        <a href="order-detail.php?id_order=<?= $each->id; ?>" title="Xem chi tiết đơn hàng">
+                            <i class="fa-solid fa-clipboard fs-1"></i>
                         </a>
-                        <a href="delete-order.php?id_order=<?= $each->id; ?>">
-                            <i class="fa-solid fa-trash-can"></i>
+                        <a href="delete-order.php?id_order=<?= $each->id; ?>" title="Xóa đơn hàng">
+                            <i class="fa-solid fa-trash-can fs-1"></i>
                         </a>
                     </td>
                 </tr>
@@ -62,43 +57,3 @@ if (isset($_GET['error'])) {
         </div>
     </main>
 </main>
-
-<!-- Modal -->
-<div class="modal fade modal-xl" id="openModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-3" id="exampleModalLabel">Thêm danh mục sản phẩm</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="action-product-category.php" method="post">
-                    <div class="mb-3">
-                        <label for="" class="form-label fs-3">Tên danh mục</label>
-                        <input type="text" class="form-control fs-3" name="name-product-category"
-                            placeholder="Tên danh mục" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="" class="form-label fs-3">Mô tả danh mục</label>
-                        <textarea class="form-control fs-3" id="" rows="3" name="desc-product-category"
-                            placeholder="Mô tả danh mục" required></textarea>
-                    </div>
-                    <select class="form-select fs-3" aria-label="Default select example" name="id_product_type">
-                        <!-- render ra loại sản phẩm -->
-                        <?php foreach ($category_type as $each) { ?>
-                        <option value="<?= $each->id; ?>"><?= $each->type; ?></option>
-                        <?php } ?>
-                        <!-- end -->
-                    </select>
-                    <!-- <button id="btnSubmit" name="submit" class="btn btn-primary">oke</button> -->
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary fs-4" data-bs-dismiss="modal">Đóng</button>
-                        <button type="submit" class="btn btn-danger fs-4" data-bs-target="submit-form"
-                            name="submit">Thêm sản
-                            phẩm</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
