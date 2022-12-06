9c640e8f74c48d662eeda07053c445c3efc8bff1
DevidMonster <mrbat905@gmail.com>
2022-12-06 18:56:41 +0700

    fix render item

--- models/m_product.php
+++ models/m_product.php
@@ -3,20 +3,20 @@
     require_once ("database.php");
     class m_product extends database{
         public function getProduct(){
-            $sql = "select * FROM product"; 
+            $sql = "select * FROM product where id_category not null"; 
             $this ->setQuery($sql);
             // lấy dữ liệu nhiều dùng 
             return $this -> loadAllRows();
         }
 
         public function getProductById($id){
-            $sql = "select * FROM product where id = ?"; 
+            $sql = "select * FROM product where id = ? and id_category not null"; 
             $this ->setQuery($sql);
             return $this -> loadRow(array($id));
         }
 
         public function getProductBySearch($search,$number_in_on_page, $clear, $order_by, $action){
-            $sql = "select * FROM product where name like '%$search%' $order_by $action limit $number_in_on_page
+            $sql = "select * FROM product where name like '%$search%' and id_category is not null $order_by $action limit $number_in_on_page
             offset $clear;"; 
             $this ->setQuery($sql);
             // lấy dữ liệu nhiều dùng 
@@ -24,26 +24,30 @@
         } 
 
         public function getfeaturedProducts(){
-            $sql = "select * from product where view_number >= 10 limit 0, 8";
+            $sql = "select * from product where view_number >= 10 and id_category is not null limit 0, 8";
             $this->setQuery($sql);
             return $this->loadAllRows();
         }  
 
         public function getNewProduct() {
-            $sql = "SELECT * FROM `product` ORDER BY id DESC LIMIT 0, 4;";
+            $sql = "SELECT * FROM `product` where id_category is not null ORDER BY id DESC LIMIT 0, 4;";
             $this->setQuery($sql);
             return $this->loadAllRows();
         }
 
         public function getProductByCateory($id_category, $search, $number_in_on_page, $clear, $order_by, $action) {
-            $sql = "SELECT * FROM `product` where id_category = $id_category And name like '%$search%' $order_by $action limit $number_in_on_page
+            $string = 'id_category is not null';
+            if($id_category != "") {
+                $string = 'id_category  = '.$id_category;
+            }
+            $sql = "SELECT * FROM `product` where $id_category  = $id_category And name like '%$search%' $order_by $action limit $number_in_on_page
             offset $clear;";
             $this->setQuery($sql);
             return $this->loadAllRows();
         }
 
         public function get_count_search($search) {
-            $sql = "select count(*) from product where name like '%$search%'";
+            $sql = "select count(*) from product where name like '%$search%' and id_category is not null";
             $this -> setQuery($sql);
             return $this->loadRecord();
         }
